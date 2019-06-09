cefb6d2b18ead6275017ad3f0cdf3fea19dada65
wind <wind@comtv.ru>
2019-06-10 01:51:18 +0300

    #

--- src/main/java/com/github/windchopper/tools/log/browser/ConnectionStageController.java
+++ src/main/java/com/github/windchopper/tools/log/browser/ConnectionStageController.java
@@ -5,5 +5,5 @@ import com.github.windchopper.common.fx.annotation.FXMLResource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
-@ApplicationScoped @FXMLResource(Globals.FXML__EDIT) @Named("PreferencesStageController") public class EditStageController extends BaseStageController {
+@ApplicationScoped @FXMLResource(Globals.FXML__EDIT) @Named("PreferencesStageController") public class ConnectionStageController extends BaseStageController {
 }
