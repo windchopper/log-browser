dac7f0c5ce6c713ba1dad64cc020c8140267e814
wind <wind@comtv.ru>
2019-06-12 01:43:09 +0300

    #

--- src/main/java/com/github/windchopper/tools/log/browser/ConnectionStageController.java
+++ src/main/java/com/github/windchopper/tools/log/browser/ConnectionStageController.java
@@ -2,7 +2,9 @@ package com.github.windchopper.tools.log.browser;
 
 import com.github.windchopper.common.fx.annotation.FXMLResource;
 import com.github.windchopper.tools.log.browser.configuration.Connection;
+import com.github.windchopper.tools.log.browser.configuration.ConnectionType;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -14,6 +16,7 @@ import java.util.Map;
 @ApplicationScoped @FXMLResource(Globals.FXML__CONNECTION) @Named("ConnectionStageController") public class ConnectionStageController extends BaseStageController {
 
     @FXML private TextField nameField;
+    @FXML private ComboBox<ConnectionType> typeBox;
     @FXML private TextField hostField;
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
@@ -22,6 +25,8 @@ import java.util.Map;
 
     @Override protected void start(Stage stage, String fxmlResource, Map<String, ?> parameters, Map<String, ?> fxmlLoaderNamespace) {
         super.start(stage, fxmlResource, parameters, fxmlLoaderNamespace);
+
+        typeBox.getItems().addAll(ConnectionType.values());
 
         connection = (Connection) parameters.get("connection");
 
--- src/main/java/com/github/windchopper/tools/log/browser/configuration/ConnectionType.java
+++ src/main/java/com/github/windchopper/tools/log/browser/configuration/ConnectionType.java
@@ -1,9 +1,33 @@
 package com.github.windchopper.tools.log.browser.configuration;
 
+import com.github.windchopper.tools.log.browser.Globals;
+
 public enum ConnectionType {
 
-    SECURE_SHELL,
-    SECURE_FILE_TRANSFER,
-    FILE_TRANSFER;
+    SECURE_SHELL(
+        Globals.bundle.getString("com.github.windchopper.tools.log.browser.secureShell.title"),
+        Globals.bundle.getString("com.github.windchopper.tools.log.browser.secureShell.description")),
+    SECURE_FILE_TRANSFER(
+        Globals.bundle.getString("com.github.windchopper.tools.log.browser.fileTransfer.title"),
+        Globals.bundle.getString("com.github.windchopper.tools.log.browser.fileTransfer.description")),
+    FILE_TRANSFER(
+        Globals.bundle.getString("com.github.windchopper.tools.log.browser.secureFileTransfer.title"),
+        Globals.bundle.getString("com.github.windchopper.tools.log.browser.secureFileTransfer.description"));
+
+    private final String title;
+    private final String description;
+
+    ConnectionType(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String title() {
+        return title;
+    }
+
+    public String description() {
+        return description;
+    }
 
 }
